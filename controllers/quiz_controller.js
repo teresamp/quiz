318d097f9ef1f08c47e069e77a2f603c6effebd5
teresamp <teresa@unizar.es>
2015-06-22 17:16:02 +0200

    Ejercicio módule 6

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,38 +1,39 @@
 // TM-in para la bd
 var models = require('../models/models.js');
 //TM-fin
+//Autoload
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find(quizId).then(
+    function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else { next(new Error('No existe quizId= '+ quizId)); }
+    }
+  ).catch(function(error) { next(error);});
+};
 
 // tm-in varias preguntas
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(function(quizes) {
-    res.render('quizes/index.ejs', { quizes: quizes});
-  })
+  models.Quiz.findAll().then
+    (function(quizes) {
+       res.render('quizes/index.ejs', { quizes: quizes});
+    }
+  ).catch(function(error) { next(error);})
 };
-
-// NO se si tengo que quitar lo demas Modulo7 varias preguntas
-
-
-//tm-fin varias preguntas
 
 
 //GET  /quizes/question  TM- JUnio 2015
 exports.show = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz) {
-   res.render('quizes/show', { quiz: quiz});
- })
+   res.render('quizes/show', { quiz: req.quiz});
 };
 
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
-
- models.Quiz.find(req.params.quizId).then(function(quiz) {
-    if (req.query.respuesta === quiz.respuesta) {
-      res.render('quizes/answer',
-           { quiz:quiz, respuesta: 'Correcta'});
-    } else {
-      res.render('quizes/answer',
-           { quiz: quiz, respuesta: 'Incorrecta'});
+    var resultado = "Respuesta incorrecta";
+    if (req.query.respuesta === req.quiz.respuesta) {
+        resultado='Respuesta correcta';
     }
-  })
+    res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
 };
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,18 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.param('quizId', quizController.load);  //Autoload :quizId
+
+
+router.get('/author', function(req, res, next) {
+  res.render('author', { author: 'Teresa', image: '/images/foto_tm.jpg' });
+});
+
+/* GET Autores de la aplicacion */
+router.get('/author', function(req, res) {
+  res.render('author');
+});
+
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
