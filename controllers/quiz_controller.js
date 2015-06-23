13b2903cc0ea9818193a277aed8fec2a55084cb8
teresamp <teresa@unizar.es>
2015-06-23 23:09:18 +0200

    Modulo8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -25,7 +25,7 @@ exports.load = function(req, res, next, quizId) {
 };
 
 // GET /quizes
-exports.index = function(req, res) {
+/* exports.index = function(req, res) {
 
 	var resultado=req.query.search;
 	if (resultado){
@@ -37,21 +37,21 @@ exports.index = function(req, res) {
 		}
 	).catch(function(error) {next(error);});
 };
-
+*/
 
 // tm-in varias preguntas  --
-/*exports.index = function(req, res) {
+exports.index = function(req, res) {
   models.Quiz.findAll().then
     (function(quizes) {
-       res.render('quizes/index.ejs', { quizes: quizes});
+       res.render('quizes/index.ejs', { quizes: quizes, errors: []});
     }
   ).catch(function(error) { next(error);})
 };
-*/
+
 
 //GET  /quizes/question  TM- JUnio 2015
 exports.show = function(req, res) {
-   res.render('quizes/show', { quiz: req.quiz});
+   res.render('quizes/show', { quiz: req.quiz , errors: []});
 };
 
 
@@ -61,12 +61,33 @@ exports.answer = function(req, res) {
     if (req.query.respuesta === req.quiz.respuesta) {
         resultado='Respuesta correcta';
     }
-    res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
+    res.render(
+      'quizes/answer',
+       { quiz: req.quiz, respuesta: resultado,
+         errors[] });
 };
 
 exports.new = function(req, res) {
      var quiz = models.Quiz.build(
        {pregunta:"Pregunta", respuesta: "Respuesta"}
-    ); 
-    res.render('quizes/new', { quiz: quiz });
+    );
+    res.render('quizes/new', { quiz: quiz, errors: [] });
 };
+
+// POST /quizes/create
+
+export.create = function(req, res) {
+    var quiz = models.Quiz.build( req.body.quiz );
+    quiz.validate()
+    .then(
+       function(err) {
+         if (err) {
+           res.render('quizes/new',{quiz:quiz,errors: err.errors});
+         } else {
+
+           quiz.save( {fields: ["pregunta", "respuesta"]}).then.(function(){
+           res.direct('/quizes')})
+         }
+      }
+    );
+  };
