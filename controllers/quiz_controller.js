f5022935d95cc8a4edd0e05c2320f7f44da18d0c
teresamp <teresa@unizar.es>
2015-06-30 16:30:16 +0200

    Update DB

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -57,21 +57,26 @@ exports.new = function(req, res) {
 };
 
 // POST /quizes/create
-exports.create = function(req, res) {
-    var quiz = models.Quiz.build( req.body.quiz );
-    console.log("Voy a crear un quiz con los valores ("+quiz['pregunta']+","+quiz['respuesta']+","+quiz['tema']);
-// Forma del foro 
+exports.create = function(req, res){
+var quiz = models.Quiz.build( req.body.quiz );
 
- var errors = quiz.validate();
- if (errors)
- {
-   res.render('quizes/new', {quiz: quiz, errors: errors});
- } else {
-   quiz // save: guarda en DB campos pregunta y respuesta de quiz
-  .save({fields: ["pregunta", "respuesta", "tema"]})
-  .then( function(){ res.redirect('/quizes')}) ;	
+var errors = quiz.validate();//ya qe el objeto errors no tiene then(
+if (errors)
+{
+var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+for (var prop in errors) errores[i++]={message: errors[prop]};	
+res.render('quizes/new', {quiz: quiz, errors: errores});
+} else {
+quiz // save: guarda en DB campos pregunta y respuesta de quiz
+.save({fields: ["pregunta", "respuesta", "tema"]})
+.then( function(){ res.redirect('/quizes')}) ;
 }
 };
+
+
+
+
+
 
 
 // GET quizes/:id/edit
