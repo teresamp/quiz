dd67f07904b2a9adef0c2cfea86ff2ce7f62e13e
teresamp <teresa@unizar.es>
2015-06-30 08:23:34 +0200

    Test 20150630-1

--- models/models.js
+++ models/models.js
@@ -45,9 +45,11 @@ var Quiz = sequelize.import(quiz_path);
 exports.Quiz =Quiz; // exportar la definición de la tabla Quiz
 
 // sequelize.sync() crea e inicializa la tabla de preguntas DB
-sequelize.sync().success(function() {
+//sequelize.sync().success(function() {
+sequelize.sync().then(function() {
   // success(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function (count) {
+  Quiz.count().then(function (count) {
+  //Quiz.count().success(function (count) {
     if(count === 0) { //la tabla se inicializa solo si está vacía
          Quiz.create({ pregunta:  'Capital de Italia',
                         respuesta: 'Roma',
